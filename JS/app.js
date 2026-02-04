69f6b8926dcd7d21352ee0382f6121121ea15d96
JeiversonChristian <jeiversonc@gmail.com>
2026-02-04 16:47:54 -0300

    Refactor: Remoção de estilos inline e ajustes no Modal
    Transferência da estilização do botão de fechar modal do HTML para o arquivo CSS externo (componentes.css), seguindo boas práticas de desenvolvimento e separação de responsabilidades.

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,20 @@
 /* --- APP PRINCIPAL --- */
 
 /* Importando as funcionalidades das outras partes (Imports) */
-/* Note que precisamos colocar o caminho e o ".js" no final */
 import iniciarLogicaBotoes from './modulos/botoes.js';
 import iniciarLogicaFormulario from './modulos/formulario.js';
+/* NOVO: Importo o módulo do modal */
+import iniciarModalPerfil from './modulos/modal.js';
 
 /* --- INICIALIZAÇÃO --- */
-/* Quando o site carregar, eu executo as funções */
 
-// 1. Inicio a lógica dos botões (vai funcionar em qualquer página que tenha botões)
+// 1. Inicio a lógica dos botões
 iniciarLogicaBotoes();
 
-// 2. Inicio a lógica do formulário (o próprio módulo verifica se o form existe)
+// 2. Inicio a lógica do formulário
 iniciarLogicaFormulario();
+
+// 3. NOVO: Inicio a lógica do modal de perfil (só vai funcionar onde tiver a foto com a classe gatilho)
+iniciarModalPerfil();
 
 console.log("Aplicação iniciada pelo Gerente (app.js)");
